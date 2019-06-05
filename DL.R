cb4879943c4a9accdd6103c463a2ebcee377b825
Dmitri <dleybman@newvisions.org>
2019-06-05 15:53:01 -0400

    another update

--- DL.R
+++ DL.R
@@ -4,9 +4,9 @@ test5 <- function(arg1= integer(), arg2= integer(), arg3= "Yes") {
   
 }
 
-test2(arg1 = 3, arg2 = 4)
-test2(3, 4)
-test2(arg2 = 4, arg = 3)
+test3(arg1 = 3, arg2 = 4)
+test3(3, 4)
+test3(arg2 = 4, arg = 3)
 
 
 test2 <-function(arg4 = character(), arg5 = integer(), arg6 = integer()) {
